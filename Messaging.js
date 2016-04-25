aefeb2fd6c89850c1872e25b1308c76be4e795ee
Matz Larsson <matzlarsson94@gmail.com>
2016-04-25 18:36:14 +0200

    Add methods for doing stuff & secret marker

--- Messaging.js
+++ Messaging.js
@@ -1,5 +1,6 @@
 var typers = [];
 var messages = [];
+var server;
 
 exports.setupMessaging = function(server){
 	server.registerOnServerStartAction(function(socket){
@@ -10,22 +11,31 @@ exports.setupMessaging = function(server){
 		setTyping(server, this, false);
 	});
 	server.registerOnSocketMessageAction('chatmsg', function(msg){
-		server.sendMessageToEveryoneElse(this, 'chatmessage', registerMessage(server, this, msg));
+		server.sendMessageToEveryoneElse(this, 'chatmessage', registerMessage(this, msg));
 	});
 	server.registerOnSocketMessageAction('istyping', function(isTyping){
 		setTyping(server, this, isTyping);
 	});
 };
 
+exports.sendMessageToAll = function(server, displayName, messageContent){
+	server.sendMessageToAll('servermessage', registerMessage(displayName, messageContent));
+};
+exports.sendMessageToSomeUsers = function(server, users, displayName, messageContent, isSecret){
+	var msg = registerMessage(displayName, messageContent, (isSecret?'secret':undefined));
+	const sendMessage = function(user){server.sendMessageToMyself(user, 'servermessage', msg);};
+	users.forEach(sendMessage);
+};
 
-function registerMessage(server, user, msg){
-	msg = {user:user.nickname, message:msg, time:new Date()};
+
+function registerMessage(user, msg, status){
+	msg = {user:user.nickname, message:msg, status:status, time:new Date()};
 	messages.push(msg);
 	return msg;
 }
 
 function getPreviousMessages(){
-	return messages;
+	return messages.filter(function(value){return value.status === undefined || value.status !== 'secret';});
 }
 
 function setTyping(server, user, isTyping){
